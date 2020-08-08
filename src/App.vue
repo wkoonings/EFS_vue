0c6284dc84e5d3067094ea5a831a25d94777edcd
willem_koonings@hotmail.com <willem_koonings@hotmail.com>
2020-08-08 17:09:57 +0200

    complete assingment

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <v-app>
+            <v-toolbar class="blue">
+                <v-toolbar-title>Eagle Finance Service</v-toolbar-title>
+                <v-toolbar-items>
+                    <v-btn flat dark @click="goHome">Home</v-btn>
+                    <v-btn flat dark @click="viewCustomers">Customers</v-btn>
+                    <v-btn flat dark @click="viewStocks">Stocks</v-btn>
+                    <v-btn flat dark @click="viewInvestments">Investments</v-btn>
+                    <v-btn flat dark v-if="!authenticated"
+                           @click="login">Log in
+                    </v-btn>
+                    <v-btn flat dark v-if="authenticated"
+                           @click="logout">Log Out
+                    </v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
+            <v-content>
+
+                <router-view/>
+            </v-content>
+        </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import router from './router';
+    import {APIService} from './http/APIService';
+    const apiService = new APIService();
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data: () => ({
+            authenticated: false,
+        }),
+
+        mounted() {
+            apiService.getCustomerList().then(response => {
+                console.log(response)
+                this.authenticated = true;
+            }).catch(error => {
+                if (error.response.status === 401) {
+                    localStorage.removeItem('isAuthenticates');
+                    localStorage.removeItem('log_user');
+                    localStorage.removeItem('token');
+                    this.authenticated = false;
+                }
+            });
+            console.log('this.authenticated--'+this.authenticated);
+        },
+
+        methods: {
+            logout() {
+                localStorage.removeItem('isAuthenticates');
+                localStorage.removeItem('log_user');
+                localStorage.removeItem('token');
+                this.authenticated = false;
+                // router.push('/');
+                window.location = "/"
+            },
+            viewCustomers() {
+                router.push('/customer-list');
+            },
+            viewInvestments() {
+                router.push('/investment-list')
+            },
+            login() {
+                router.push("/auth");
+            },
+            viewStocks() {
+                router.push('/stock-list')
+            },
+            goHome() {
+                router.push('/');
+            }
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 0px;
+
+    }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueSession from 'vue-session'
+
+Vue.use(Vuetify, {
+    icons: {
+        iconfont: 'md'
+    }
+})
+Vue.use(VueSession)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    el: '#app',
+    router,
+    render: h => h(App),
 }).$mount('#app')
